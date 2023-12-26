f5502aca875f2b7c5530e2ab205eccf974217e54
fan_mac <fan.jinlong@qq.com>
2023-12-26 21:58:13 +0800

    重构豆瓣 class code for http request

--- hello_flutter/lib/dio/dio_page.dart
+++ hello_flutter/lib/dio/dio_page.dart
@@ -26,36 +26,6 @@ class JLContentBody extends StatefulWidget {
 }
 
 class _JLContentBodyState extends State<JLContentBody> {
-  @override
-  void initState() {
-    super.initState();
-    // dio请求
-    // 1.创建dio
-    // final dio = Dio();
-    // 发起请求
-    // 推荐这个测试网址
-    // dio.get("https://httpbin.org/get").then((value) {
-    //   print(value);
-    // });
-    // dio.post("https://httpbin.org/post").then((value) {
-    //   print(value);
-    // });
-    // 真实开发是：参数-> 拦截器(封装)
-//    只要用到第三方库，建议都做一层封装
-    HttpRequest.request("https://httpbin.org/get", parames: {"name": "any"},
-        inter: InterceptorsWrapper(onRequest: (request, handler) {
-          return handler.next(request);
-        })).then((value) {
-      print("==json==");
-      print(value);
-      print("type of value ${value.runtimeType}");
-      //输出第一个用户的姓名
-      print(value["origin"]);
-      // json转模型
-      var model = HttpEchoModel.fromJson(value);
-      print(model.url);
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
